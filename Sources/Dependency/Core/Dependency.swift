2bbf14ff2d24db2539134b30fb33c55115184f4d
김학철 <hak20116@gmail.com>
2026-02-11 15:32:25 +0900

    public

--- Sources/Dependency/Core/Dependency.swift
+++ Sources/Dependency/Core/Dependency.swift
@@ -4,26 +4,28 @@ import SwiftUI
 import Combine
 
 // 1. 의존성 키 프로토콜
-protocol DependencyKey {
+public protocol DependencyKey {
     associatedtype Value: Sendable
     static var liveValue: Value { get }
     static var testValue: Value { get }
 }
 
 // 2. 의존성 저장소
-struct DependencyValues: Sendable {
+public struct DependencyValues: Sendable {
     nonisolated(unsafe) private static var current = DependencyValues()
     
     // Task-local 저장소 (Swift Concurrency 전파 핵심)
-    @TaskLocal static var localValues: DependencyValues?
+    @TaskLocal public static var localValues: DependencyValues?
 
     private var storage: [ObjectIdentifier: any Sendable] = [:]
 
-    static var currentValues: DependencyValues {
+    public static var currentValues: DependencyValues {
         localValues ?? current
     }
+    
+    public init() {}
 
-    subscript<K: DependencyKey>(key: K.Type) -> K.Value {
+    public subscript<K: DependencyKey>(key: K.Type) -> K.Value {
         //get { storage[ObjectIdentifier(key)] as? K.Value ?? K.liveValue }
         get {
             if let value = storage[ObjectIdentifier(key)] as? K.Value {
@@ -36,30 +38,28 @@ struct DependencyValues: Sendable {
 }
 
 extension DependencyValues {
-    static var isRunningForPreviews: Bool {
+    public static var isRunningForPreviews: Bool {
         ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
     }
     
-    static var isRunningTests: Bool {
+    public static var isRunningTests: Bool {
         ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
     }
     
-    static var useTestDefaults: Bool {
+    public static var useTestDefaults: Bool {
         return isRunningForPreviews || isRunningTests
     }
 }
 
 @propertyWrapper
-struct Dependency<Value>: @unchecked Sendable {
+public struct Dependency<Value>: @unchecked Sendable {
     private let keyPath: KeyPath<DependencyValues, Value>
     
-    var wrappedValue: Value {
+    public var wrappedValue: Value {
         DependencyValues.currentValues[keyPath: keyPath]
     }
     
-    init(_ keyPath: KeyPath<DependencyValues, Value>) {
+    public init(_ keyPath: KeyPath<DependencyValues, Value>) {
         self.keyPath = keyPath
     }
 }
-
-
